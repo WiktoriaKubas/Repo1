d5665254699b37f160ad60bdf0e1ab250b92055d
Wiktoria Kubas <wiktoria.kubas@alfa1.smcebi.us.edu.pl>
2016-11-21 10:11:26 +0000

    ZMIANA: Srednia_ar.py z funkcja sprawdzajaca

--- Srednia_ar.py
+++ Srednia_ar.py
@@ -5,8 +5,25 @@
 
 def srednia(lista):
     """Oblicza srednia arytmetyczna ciagu liczb podanych jako lista"""
-    x=0
-    for k in lista:
-        x+=k
-    return(x/len(lista))
+    if sprawdz(lista)==True:
+        x=0
+        for k in lista:
+            x+=k
+        return("RESULT: "+str(x/len(lista)))
+    else:
+        return("FAILED: WRONG TYPES")
 
+def sprawdz(lista):
+    ret=False
+    if type(lista)==list or type(lista)==tuple:
+        for k in lista:
+            if type(k)==int or type(k)==float:
+                ret=True
+            else:
+                return False
+        return ret
+    else:
+        return False
+
+print(srednia([1,2,"a",3]))
+print(srednia([1,2,3,4]))
