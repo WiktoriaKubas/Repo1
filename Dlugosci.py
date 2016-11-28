bcc47ec98ee7c11e5c83eef010d14142bde887c8
Wiktoria Kubas <wiktoria.kubas@alfa1.smcebi.us.edu.pl>
2016-11-28 10:51:56 +0000

    MODYFIKACJA: Dlugosci.py na branchu aaa

--- Dlugosci.py
+++ Dlugosci.py
@@ -11,7 +11,11 @@ class PROG():
         source=open(name+".txt").read()
         IN=source.split(" ")
         dic={}
-        for i in IN:
+        for i in IN
+	    if(i[-1]=="," or i[-1]=="."):
+	        i=i[0:(len(i)-1)]
+            elif(i[0]=="," or i[0]=="."):
+                i=i[1:(len(i))]
             if len(i) in dic:
                 dic[len(i)]=dic[len(i)]+1
             else:
