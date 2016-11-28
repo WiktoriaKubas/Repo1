1911b0c2302c9f7ffdff7480d7ffe71276a5f0e0
Wiktoria Kubas <wiktoria.kubas@alfa1.smcebi.us.edu.pl>
2016-11-28 10:02:43 +0000

    UTWORZENIE: Dlugosci.py

---
+++ Dlugosci.py
@@ -0,0 +1,16 @@
+class PROG():
+    def __init__(self):
+        pass
+
+    def funkcja(self,name):
+        source=open(name+".txt").read()
+        IN=source.split(" ")
+        dic={}
+        for i in IN:
+            if len(i) in dic:
+                dic[len(i)]=dic[len(i)]+1
+            else:
+                dic[len(i)]=1
+        return dic
+                    
+P=PROG()
