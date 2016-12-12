f378fa185171168d95b5d5fbe669e59334f722cc
Mateusz.Lindel <mateusz.lindel@smcebi.edu.pl>
2016-12-12 12:00:26 +0100

    rysowanie histogramu dla duzych danych

--- Rysuj.py
+++ Rysuj.py
@@ -4,10 +4,20 @@ class Rysuj:
 	"""
 	def __init__(self, dic):
 		self.dic = dic
+
 	def rysujhisto(self):
 		""" funkcja rysujaca histogram
 
 		"""
+		ldic = []
+		histogram = []
+		for check in self.dic:
+			if self.dic[check] > 70:
+				return self.rysujduzyhisto()
+		return self.rysujmalehisto()
+	
+	def rysujmalehisto(self):
+
 		ldic = []
 		histogram = []
         	for i in self.dic:
@@ -26,6 +36,34 @@ class Rysuj:
 			histogram.append(j)
 			histogram.append(temp)
 		for x in range(0, len(histogram), 2):
-			print str(histogram[x]) + " : " + histogram[x+1]					
+			print str(histogram[x]) + " : " + str(histogram[x+1])					
 		return histogram	
+	
+	def rysujduzyhisto(self):
+                ldic = []
+                histogram = []
+                for i in self.dic:
 
+                        ldic.append(i)
+                ldic.sort()
+
+                for j in ldic:
+                        counter = self.dic[j]
+                        n = 0
+                        temp = ""
+                        while n < counter:
+                                temp += "="                     
+				if "==========" in temp:		
+					temp = temp.replace("==========", "#")
+				if "##########" in temp:
+					temp = temp.replace("##########", "*")
+				if "**********" in temp:
+					temp = temp.replace("**********", "$")
+				n += 1	
+                        histogram.append(j)
+			histogram.append(temp)
+				
+		for x in range(0, len(histogram), 2 ):
+			print str(histogram[x]) + " : " + histogram[x+1]					
+
+                return histogram   
--- testRys.py
+++ testRys.py
@@ -1,4 +1,13 @@
 from Rysuj import Rysuj
 
 P=Rysuj({3 : 5, 5: 3, 1: 1, 2 : 3})
-print P.rysujhisto()
+P2=Rysuj({10 : 100, 71: 200, 30: 30})
+#print P.rysujhisto()
+temp = ""
+n = 20
+while n < 21:
+	temp += "="
+	temp.replace("==========", "#")
+	n+=1
+print temp
+print P2.rysujhisto()
