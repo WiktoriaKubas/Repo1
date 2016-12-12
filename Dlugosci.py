5b7094cad94b291bae91d02f44578ff96230092b
Wiktoria Kubas <wiktoria.kubas@alfa1.smcebi.us.edu.pl>
2016-12-12 10:45:01 +0000

    Poprawka

--- Dlugosci.py
+++ Dlugosci.py
@@ -17,22 +17,7 @@ class PROG():
                 dic[len(i)]=dic[len(i)]+1
             else:
                 dic[len(i)]=1
-        while True:
-            ask=input("Wyswietlic wyniki(1) czy zapisac do pliku(2)?")
-            if ask=="1":
-                return dic
-            elif ask=="2":
-                target=input("Podaj nazwe pliku(bez rozszerzenia): ")
-                temp=dic.items()
-                string=""
-                for i in temp:
-                    string+=(str(i[0])+": "+str(i[1])+"\n")
-                out=open(target+".txt",'w')
-                out.write(string)
-                out.close()
-                return(target+".txt")
-            else:
-                print("Nieprawidlowa komenda!")
+        return dic
                     
 P=PROG()
 P.funkcja("Dlugosci")
