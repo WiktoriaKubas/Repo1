84fd580505f64ebefa95660804c810a902514443
Wiktoria Kubas <wiktoria.kubas@alfa1.smcebi.us.edu.pl>
2016-12-12 10:19:26 +0000

    MODYFIKACJA: Dlugosci.py na wybor czy chce my wynik jako wyswietlona lista czy w pliku

--- Dlugosci.py
+++ Dlugosci.py
@@ -4,27 +4,36 @@ class PROG():
         pass
 
     def funkcja(self,name):
-	"""
-	:name file
-	:return dictionary 
-	"""
         source=open(name+".txt").read()
+        source=source.replace(",","")
+        source=source.replace(".","")
+        source=source.replace(";","")
+        source=source.replace(":","")
+        source=source.replace("\n"," ")
         IN=source.split(" ")
         dic={}
-        IN=IN.replace(",","")
-        IN=IN.replace(".","")
-        IN=IN.replace(";","")
-        IN=IN.replace(":","")
-        IN=IN.replace("\n"," ")
         for i in IN:
-            if(i[-1]=="," or i[-1]=="."):
-               i=i[0:(len(i)-1)]
-            elif(i[0]=="," or i[0]=="."):
-               i=i[1:(len(i))]
             if len(i) in dic:
                 dic[len(i)]=dic[len(i)]+1
             else:
                 dic[len(i)]=1
-        return dic
+        while True:
+            ask=input("Wyswietlic wyniki(1) czy zapisac do pliku(2)?")
+            if ask=="1":
+                print(dic)
+                return
+            elif ask=="2":
+                target=input("Podaj nazwe pliku(bez rozszerzenia): ")
+                temp=dic.items()
+                string=""
+                for i in temp:
+                    string+=(str(i[0])+": "+str(i[1])+"\n")
+                out=open(target+".txt",'w')
+                out.write(string)
+                out.close()
+                return(target+".txt")
+            else:
+                print("Nieprawidlowa komenda!")
                     
 P=PROG()
+P.funkcja("Dlugosci")
